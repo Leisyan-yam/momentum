965c7ccbe86c5a4cb3f3d14a3ec4b86020c08b19
Leisyan Yamaltdinova <n.leisyan@yandex.ru>
2023-02-26 23:56:10 +0300

    feat: add quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,7 +1,19 @@
 export default async function getQuotes() {
 const quotes = 'data.json';
-console.log(quotes);
   const res = await fetch(quotes);
   const data = await res.json(); 
   console.log(data);
-  }
+  
+  const changeQuote = document.querySelector('.change-quote');
+  const quote = document.querySelector('.quote');
+  const author = document.querySelector('.author')
+  quote.textContent = data[0].text;
+  author.textContent = data[0].author;
+  changeQuote.addEventListener('click',  function changeQuote () {
+    let randomNumber = Math.floor(Math.random()*15 + '1')
+    quote.textContent = data[randomNumber].text;
+    author.textContent = data[randomNumber].author;
+  })
+  }  
+  
+ 
