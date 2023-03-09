b2e64bd19c78943956d0c5f1454664d027efad79
Leisyan Yamaltdinova <n.leisyan@yandex.ru>
2023-03-09 23:29:48 +0300

    feat: add extended audioplayaer

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -2,76 +2,127 @@ import playlist from "./playlist";
 const playBtn = document.querySelector('.play');
 const playNextBtn = document.querySelector('.play-next');
 const playPrevBtn = document.querySelector('.play-prev');
-const playListContainer = document.querySelector('.play-list')
+const playListContainer = document.querySelector('.play-list');
+const musicTime = document.querySelector('.current-time');
+const musicDuration = document.querySelector('.song-duration');
+const seekBar = document.querySelector('.seek-bar');
+const musicName = document.querySelector('.name-audio');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBtn = document.querySelector('.volumeBtn');
 
 const audio = new Audio();
-let isPlay = false;
-let num = 0
+let num = 0;
+
+
+
+const setPlayer = () => {
+  seekBar.value = 0; 
+  audio.volume = .2;
+  volumeRange.value = 20;
+  audio.src = playlist[num].src;
+  musicTime.innerHTML = '00:00';
+  audio.play();
+  setTimeout(() => {
+    seekBar.max = audio.duration;
+    musicDuration.innerHTML = formatTime(audio.duration);
+  }, 300);
+  setInterval(()=>{
+    seekBar.value = audio.currentTime;
+    musicTime.innerHTML = formatTime(audio.currentTime);
+    if (Math.floor(audio.currentTime)== Math.floor(seekBar.max)){
+      playNextBtn.click()
+     }
+  }, 500)
+  musicName.textContent = playlist[num].title;
+  document.querySelectorAll('.play-item').forEach(Element => {
+    if ( Element.textContent == playlist[num].title) {
+      Element.classList.add('item-active');
+    }
+    else {
+      Element.classList.remove('item-active');
+    }
+  })
+}
+
+
+
 
 for (let i=0; i < playlist.length; i++){
     const li = document.createElement('li');
     li.classList.add('play-item');
     li.textContent = playlist[i].title;
     playListContainer.append(li);
-    }
-
-    function playAudio() {
-        if(!isPlay) {
-          audio.src = playlist[num].src;
-          audio.currentTime = 0;
-          audio.play();
-          playBtn.classList.add('pause');
-          document.querySelectorAll('.play-item').forEach(element => {
-            if (element.innerHTML == playlist[num].title) {
-                element.classList.add('item-active')
-            }
-            else {
-                element.classList.remove('item-active')
-            }
-          });
-          isPlay = true;
-        }
-        else {
-          audio.pause()
-          isPlay=false;
-          playBtn.classList.remove('pause');
-        } 
-      }
-
-      
+}
 
 
 
 function playNext() {
-   num += 1;   
-   if (num > playlist.length-1) {
+  num += 1;   
+  if (num > playlist.length-1) {
     num = 0
-   }
-   isPlay = false
-   playAudio() 
+  }
+  playBtn.classList.add('pause')
+  setPlayer(); 
 }
 
 
 function playPrev() {
-    num -= 1;   
-    if (num < 0) {
-     num = playlist.length-1
-    }
-    isPlay = false
-    playAudio() 
-    
- }
- 
+  num -= 1;   
+  if (num < 0) {
+    num = playlist.length-1
+  }
+  playBtn.classList.add('pause')
+  setPlayer(); 
+}
 
+
+const formatTime = (time) => {
+  let min = Math.floor(time / 60);
+  if ( min < 10 ) {
+    min = `0${min}`
+  };
+  let sec  = Math.floor(time % 60)
+  if ( sec < 10 ) {
+    sec = `0${sec}`
+  }
+  return `${min} : ${sec}`
+}  
+
+
+const muteAudio = () => {
+  if (!volumeBtn.classList.contains('volume-Off')) {
+    audio.volume = 0;
+    volumeBtn.classList.add('volume-Off')
+  }
+  else {
+    volumeBtn.classList.remove('volume-Off');
+    audio.volume = 1;
+  }
+}
+
+function changeVolume () {
+  audio.volume = volumeRange.value/100
+}
 
 
 export default function showPlayList() {
 playNextBtn.addEventListener('click', playNext)
 playPrevBtn.addEventListener('click', playPrev)
-playBtn.addEventListener('click', playAudio)
-};
-
-
-
-
+volumeRange.addEventListener('change', changeVolume)
+volumeBtn.addEventListener('click', muteAudio)
+ seekBar.addEventListener('change' , () => {
+   audio.currentTime = seekBar.value;
+ })
+ 
+playBtn.addEventListener('click', () => {
+  if (!playBtn.classList.contains('pause')) {
+  setPlayer()
+  playBtn.classList.add('pause');
+  }
+  else {
+  playBtn.classList.remove('pause');
+  audio.pause();
+  }
+})
+}
 
